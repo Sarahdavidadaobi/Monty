275b88e0dbe776d417b0529a14eecd4eefb2362a
Sarahdavidadaobi <davidsarah008@gmail.com>
2023-07-30 06:25:12 -0700

    Monty

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * main - Starts the program
+ * @argc : no of command line arguments
+ * @argv : command line arguments
+ *
+ * Return: on success - EXIT_SUCCESS, on failure - EXIT_FAILURE
+ */
+int main(int argc, char **argv)
+{
+	FILE *fp;
+	int exit_value;
+
+	exit_value = EXIT_FAILURE;
+	if (argc != 2)
+		printf("USAGE: monty file\n");
+	else
+		{
+			fp = fopen(argv[1], "rt");
+			if (fp == NULL)
+				printf("Error: Can't open file %s\n", argv[1]);
+			else
+			{
+					exit_value = (monty_parse(fp) == 1 ? EXIT_SUCCESS : EXIT_FAILURE);
+					fclose(fp);
+			}
+		}
+	return (exit_value);
+}
